f143d1b6833d630a7cffad266bc3e528fdec3eae
iphone513s <43226563+iphone513s@users.noreply.github.com>
2025-05-09 10:33:28 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -19,7 +19,7 @@ function gotHands(results) {
 }
 
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(640, 480); // 產生一個畫布
   video = createCapture(VIDEO, { flipped: true });
   video.hide();
 
@@ -27,7 +27,29 @@ function setup() {
   handPose.detectStart(video, gotHands);
 }
 
+// 假設 hand.keypoints 是一個包含 21 個點的陣列，每個點有 x, y 屬性
+// hands 是一個陣列，包含左右手 hand 物件
+
+function drawHandLines(hand) {
+  const groups = [
+    [0, 1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16],
+    [17, 18, 19, 20]
+  ];
+
+  for (let group of groups) {
+    for (let i = 0; i < group.length - 1; i++) {
+      let p1 = hand.keypoints[group[i]];
+      let p2 = hand.keypoints[group[i + 1]];
+      line(p1.x, p1.y, p2.x, p2.y);
+    }
+  }
+}
+
 function draw() {
+  background(220);
   image(video, 0, 0);
 
   // Ensure at least one hand is detected
@@ -48,6 +70,7 @@ function draw() {
           noStroke();
           circle(keypoint.x, keypoint.y, 16);
         }
+        drawHandLines(hand);
       }
     }
   }
